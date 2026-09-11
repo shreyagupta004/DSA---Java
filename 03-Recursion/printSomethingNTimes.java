33ccad4d2ed44c23c03952896f24c7f951499c87
Shreya Gupta <iamshreyagupta13@gmail.com>
2026-09-11 18:40:52 +0530

    recursion

---
+++ 03-Recursion/printSomethingNTimes.java
@@ -0,0 +1,27 @@
+public class printSomethingNTimes{
+
+    public static void main(String[] args) {
+        
+      //  printOne();
+
+        printCount(0);
+
+    }
+
+    // public static void printOne(){
+    //     System.out.println("1");
+    //     printOne();
+    // }
+    
+    public static void printCount(int count){
+        
+        if(count == 5){
+            return;
+        }
+        System.out.println(count);
+        count++;
+        printCount(count);
+        
+
+    }
+ }  
