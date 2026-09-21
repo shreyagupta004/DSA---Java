115d787f7d8dbce0015026de917f8c008050adb8
Shreya Gupta <iamshreyagupta13@gmail.com>
2026-09-21 21:20:31 +0530

    left rotate by 1 place

---
+++ 06-Arrays/Easy/05-LeftRotateByOnePlace/OptimalSolution.java
@@ -0,0 +1,17 @@
+public class OptimalSolution {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4,5,6};
+        rotate(nums);
+        for(int i = 0 ; i < nums.length ; i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+    public static  void rotate(int[] nums) {
+        
+       int temp = nums[0];
+        for(int i = 1 ; i < nums.length ; i++){
+            nums[i - 1] = nums[i];
+        }
+        nums[nums.length - 1] = temp;
+    }
+}
